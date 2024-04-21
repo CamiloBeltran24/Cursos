264082850d0d7adcba4cbdb1e25d0a2afdf7ac3f
Christian Beltran <cambelf.dev@gmail.com>
2024-04-20 21:30:04 -0500

    UPDATE: 8-objetos

--- Platzi/javascript-desde-cero/8-objetos/5-this.js
+++ Platzi/javascript-desde-cero/8-objetos/5-this.js
@@ -2,3 +2,19 @@
  * this es una palabra reservada que hace referencia al objeto actual
  */
 
+
+class Persona {
+  constructor( nombre, edad ){
+    this.nombre = nombre; // this hace referencia al objeto actual
+    this.edad = edad;
+  }
+}
+
+const persona1 = new Persona('Juan', 30); // new crea un nuevo objeto de la clase Persona, toma los argumentos del constructor y los asigna a las propiedades del objeto
+
+
+persona1.nuevoMetodo = function(){
+  console.log(`Hola mi nombre es ${this.nombre}`);
+}
+
+persona1.nuevoMetodo(); // Hola mi nombre es Juan
