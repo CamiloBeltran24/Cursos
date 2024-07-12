1fefa3ec62f85ea9e64f691f3f913a714ba9ebec
Christian Beltran <cambelf.dev@gmail.com>
2024-07-11 22:58:41 -0500

    Update: Udemy: react-cero-a-experto: seccion4 pruebas toEqual

---
+++ Udemy/react-de-cero-a-experto-hooks-y-mern/seccion4-primeros-pasos-en-react/counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,31 @@
+import { getUser } from "../../src/base-pruebas/05-funciones";
+import { getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
+
+
+describe('Esta es la prueba de 05-funciones.js', () => {
+    test('Debe retorar un objeto', () => {
+
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+        console.log(user);
+
+        expect( testUser ).toEqual( user )
+
+    });
+
+    test('getUsuarioActivo Retorna un objeto', () => {
+        const name = 'Christian';
+
+
+        const usuario = getUsuarioActivo(name)
+
+        expect( usuario ).toEqual( {
+            uid: 'ABC567',
+            username: name
+        } )
+    })
+})
